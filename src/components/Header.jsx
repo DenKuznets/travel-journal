420cb3e19c32ab6f8f3e306efcc244947a5504d4
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-30 16:49:23 +0300

    end of the day commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header>
+      <img src="../public/assets/travel-logo.png" alt="earth img" />
+      <p>my travel journal.</p>
+    </header>
+  );
+  
+}

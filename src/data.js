6882ad1d788efbce848119b3c352e2d9ced27e63
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-31 13:19:34 +0300

    complete

--- src/data.js
+++ src/data.js
@@ -1,35 +1,54 @@
 const data = [
   {
-    title: "Burj Khalifa",
+    title: "Expo 2020",
     location: "UAE, Dubai",
-    googleMapsUrl: "https://goo.gl/maps/VdB9ZDe1fgKTN3TW9",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    googleMapsUrl: "https://goo.gl/maps/ZZNiGjpomt2t1pqt8",
+    startDate: "10 Feb, 2022",
+    endDate: "20 Dec, 2022",
     description:
-      "The Burj Khalifa is a skyscraper in Dubai, United Arab Emirates.  It is known for being the world's tallest building. With a total height of 829.8 m (2,722 ft, or just over half a mile) and a roof height (excluding antenna, but including a 242.6 m spire) of 828 m.",
-    imageUrl: "burj-khalifa.jpg",
+      "Expo 2020 was a World Expo hosted by Dubai, in the United Arab Emirates, from 1 October 2021 to 31 March 2022. The main site of Expo 2020 Dubai was a 438-hectare area (1083 acres) located between the cities of Dubai and Abu Dhabi, near Dubai's southern border with Abu Dhabi.",
+    imageUrl: "expo-2020.jpg",
+  },
+  {
+    title: "Miracle Garden",
+    location: "UAE, Dubai",
+    googleMapsUrl: "https://goo.gl/maps/GJgCKkni7jMEU3NQ8",
+    startDate: "10 Feb, 2022",
+    endDate: "20 Dec, 2022",
+    description:
+      "The Dubai Miracle Garden is a flower garden located in the district of Dubailand, Dubai, United Arab Emirates. It occupies over 72,000 square metres (780,000 sq ft), making it the world's largest natural flower garden, featuring over 50 million flowers and 250 million plants",
+    imageUrl: "miracle-garden.jpg",
   },
   {
     title: "Burj Khalifa",
     location: "UAE, Dubai",
     googleMapsUrl: "https://goo.gl/maps/VdB9ZDe1fgKTN3TW9",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    startDate: "14 Jan, 2019",
+    endDate: "21 Jan, 2019",
     description:
       "The Burj Khalifa is a skyscraper in Dubai, United Arab Emirates.  It is known for being the world's tallest building. With a total height of 829.8 m (2,722 ft, or just over half a mile) and a roof height (excluding antenna, but including a 242.6 m spire) of 828 m.",
     imageUrl: "burj-khalifa.jpg",
   },
   {
-    title: "Burj Khalifa",
-    location: "UAE, Dubai",
-    googleMapsUrl: "https://goo.gl/maps/VdB9ZDe1fgKTN3TW9",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Ferrari World",
+    location: "UAE, Abu Dhabi",
+    googleMapsUrl: "https://goo.gl/maps/E4V2LPRaQy6r4XeaA",
+    startDate: "14 Jan, 2019",
+    endDate: "21 Jan, 2019",
     description:
-      "The Burj Khalifa is a skyscraper in Dubai, United Arab Emirates.  It is known for being the world's tallest building. With a total height of 829.8 m (2,722 ft, or just over half a mile) and a roof height (excluding antenna, but including a 242.6 m spire) of 828 m.",
-    imageUrl: "burj-khalifa.jpg",
+      "Ferrari World Abu Dhabi is a mostly indoors theme park located on Yas Island in Abu Dhabi, United Arab Emirates. It is the world’s first Ferrari-themed park and features Formula Rossa, the world's fastest roller coaster.",
+    imageUrl: "ferrari-park.jpg",
   },
-
+  {
+    title: "Khao Kheow Open Zoo",
+    location: "Thailand",
+    googleMapsUrl: "https://goo.gl/maps/5M6TR6hiV4xUhn5X9",
+    startDate: "15 Dec, 2017",
+    endDate: "21 Dec, 2017",
+    description:
+      "Khao Kheow Open Zoo Thai is a large zoo in Thailand. It covers an area of about 2,000 acres and contains more than 8,000 animals from more than 300 species.",
+    imageUrl: "khao-kheow.jpg",
+  },
 ];
 
 export default data;

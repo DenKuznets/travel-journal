2b77dd928d6578b32fe0951474e6817a268c414e
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-31 10:39:03 +0300

    card jsx added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,32 @@
 import './Card.css'
+const assetsUrl = '../../public/assets/';
 
 export default function Card(props) {
   console.log(props);
   return (
     <div className="card-container">
-      <div classname='img-container'>
-        <img className='card-img' src={`../../public/assets/${props.imageUrl}`} alt={props.title} />
+      <div className="img-container">
+        <img
+          className="card-img"
+          src={`${assetsUrl}${props.imageUrl}`}
+          alt={props.title}
+        />
+      </div>
+      <div className="text-container">
+        <div className="location-container">
+          <img
+            className="geo-symbol"
+            src={`${assetsUrl}geo.png`}
+            alt="geo.png"
+          />
+          <h2 className="country"> {props.location}</h2>
+          <a className="maps" href={props.googleMapsUrl}>
+            View on Google Maps
+          </a>
+        </div>
+        <h1 className='Title'>{props.title}</h1>
+        <h3 className='date'>{props.startDate} - {props.endDate}</h3>
+        <p className='description'>{props.description}</p>
       </div>
     </div>
   );
